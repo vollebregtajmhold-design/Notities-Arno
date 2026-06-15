a403c0083a8268dc7fc150530c051d3ccc955c78
vollebregtajmhold-design <vollebregt.ajmhold@gmail.com>
2026-06-15 21:16:01 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then((clients) => {
+      for (const client of clients) {
+        if ('focus' in client) return client.focus();
+      }
+      if (self.clients.openWindow) return self.clients.openWindow('./');
+    })
+  );
+});
